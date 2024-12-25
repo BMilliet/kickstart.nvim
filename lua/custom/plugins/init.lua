35b452fe3e175a58dcf3d383d2d5281338d90767
BMilliet <brunomilliet89@gmail.com>
2024-12-25 14:04:13 +0000

    feat(master): added window navigation keymaps. added oil plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,4 +3,15 @@
 --
 -- See the kickstart.nvim README for more information
 
-return {}
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+}
