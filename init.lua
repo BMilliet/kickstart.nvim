fa5e317f3788cf9127fab953d8e57b95792c0b28
BMilliet <brunomilliet89@gmail.com>
2025-02-12 15:50:08 +0000

    feat(master): added custom types for groovy

--- init.lua
+++ init.lua
@@ -218,6 +218,12 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
   command = 'set filetype=ruby',
 })
 
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  desc = 'Set filetypes for groovy',
+  pattern = { '*.dsl' },
+  command = 'set filetype=groovy',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
